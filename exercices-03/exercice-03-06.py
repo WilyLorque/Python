fb0944336bb4babcf162291a4230ed5d69cbc3d4
wiliam <wiliam.trioullier@gmail.com>
2022-12-12 16:37:30 +0100

    Ajout des exercices
    Exercices compléter

---
+++ exercices-03/exercice-03-06.py
@@ -0,0 +1,21 @@
+# exo 3.6
+# La formule suivante permet de convertir des « miles » en mètres :
+#
+# miles * 1609.344 = mètres
+#
+# Bob est en Angleterre.
+# On lui dit que la boulangerie la plus proche est à 3 miles.
+# Calculez la distance en mètres avec la variable `miles` puis stockez le résultat dans la variable `meters`.
+# Affichez un résultat arrondi de la distance en mètres avec la fonction `round()`.
+# Convertissez les mètres en kilo mètres puis stockez le résultat dans la variable `km`.
+# Affichez un résultat arrondi de la distance en kilo mètre avec la fonction `round()`.
+
+miles = 3
+
+# réponse 3.6
+
+meters = 1609.344 * miles
+print(round(meters, 0))
+
+km = meters / 1000
+print(round(km, 0))
